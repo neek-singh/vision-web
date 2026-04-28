86fc0e5f1f770567ebc2d62fbe57f74be5bea8f8
Neek Singh <ishwarsingh04080@gmail.com>
2026-04-28 21:36:42 +0530

    feat: add Google Tag Manager script and noscript

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNH8M4KH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <StructuredData />
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-SNT7CK2VDP"
@@ -60,6 +70,17 @@ export default function RootLayout({
             gtag('config', 'G-SNT7CK2VDP');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KNH8M4KH');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Basic page structure with Flexbox to keep footer at bottom */}
         <div className="flex flex-col min-h-screen">
           <Header />
